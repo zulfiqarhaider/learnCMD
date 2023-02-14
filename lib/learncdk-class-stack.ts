e799d3d0d143ea37cc3cfee766bb9319586dfb1a
zulfiqarhaider <zulfiqar.had@gmail.com>
2023-02-14 13:00:22 +1300

    1st commit
    to my repo

--- lib/learncdk-class-stack.ts
+++ lib/learncdk-class-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LearncdkClassStack extends cdk.Stack {
@@ -7,6 +10,19 @@ export class LearncdkClassStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const mybucket = new s3.Bucket(this, "muBucket", {
+
+      bucketName: "workshop-cdk-typescript-zhaider"
+    })
+
+    const mylambda = new lambda.Function(this, "myLambdafunction", {
+
+      code: lambda.Code.fromAsset('./test'),
+      handler: "test.index",
+      runtime: lambda.Runtime.PYTHON_3_8,
+    }
+    )
+    mybucket.grantRead(mylambda)
 
     // example resource
     // const queue = new sqs.Queue(this, 'LearncdkClassQueue', {
